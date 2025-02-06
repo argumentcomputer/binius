6928690250b2ac4cd77ccbfbe95bbafe7174e7c9
Artem Storozhuk <storojs72@gmail.com>
2025-02-06 20:01:32 +0200

    example: Projected / Repeated columns usage (#6)
    * example: Add example of Projected column usage
    
    * example: Add example of Repeated column usage
    
    * example: Add example of ZeroPadded column usage

---
+++ examples/acc-zeropadded.rs
@@ -0,0 +1,45 @@
+use binius_circuits::{builder::ConstraintSystemBuilder, unconstrained::unconstrained};
+use binius_core::constraint_system::validate::validate_witness;
+use binius_field::{arch::OptimalUnderlier, BinaryField128b, BinaryField8b};
+
+type U = OptimalUnderlier;
+type F128 = BinaryField128b;
+type F8 = BinaryField8b;
+
+const LOG_SIZE: usize = 4;
+
+fn main() {
+	let allocator = bumpalo::Bump::new();
+
+	let mut builder = ConstraintSystemBuilder::<U, F128>::new_with_witness(&allocator);
+
+	let bytes = unconstrained::<U, F128, F8>(&mut builder, "bytes", LOG_SIZE).unwrap();
+
+	// Height of ZeroPadded column can't be smaller than input one.
+	// If n_vars equals to LOG_SIZE, then no padding is required,
+	// the ZeroPadded column will have same length as input one, so we use bigger number
+	let n_vars = 5usize;
+	let zeropadded = builder
+		.add_zero_padded("zeropadded", bytes, n_vars)
+		.unwrap();
+
+	if let Some(witness) = builder.witness() {
+		let input_values = witness.get::<F8>(bytes).unwrap().as_slice::<F8>();
+
+		let mut zeropadded_witness = witness.new_column::<F8>(zeropadded);
+		let zeropadded_values = zeropadded_witness.as_mut_slice::<F8>();
+
+		// padding naturally happens in the end, so we just copy input data to ZeroPadded column
+		zeropadded_values[..input_values.len()].copy_from_slice(input_values);
+
+		assert_eq!(zeropadded_values.len(), 2usize.pow(n_vars as u32));
+		assert!(n_vars >= LOG_SIZE);
+		let zeroes_to_pad = 2usize.pow(n_vars as u32) - 2usize.pow(LOG_SIZE as u32);
+		assert_eq!(zeroes_to_pad, zeropadded_values.len() - input_values.len());
+	}
+
+	let witness = builder.take_witness().unwrap();
+	let cs = builder.build().unwrap();
+
+	validate_witness(&cs, &[], &witness).unwrap();
+}
