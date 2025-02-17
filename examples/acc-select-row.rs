28fe6fee689e3d31f267d4393c04571fe82f4697
Artem Storozhuk <storojs72@gmail.com>
2025-02-17 15:41:45 +0200

    example: Add example of Transparent (SelectRow) column usage

---
+++ examples/acc-select-row.rs
@@ -0,0 +1,35 @@
+use binius_circuits::builder::ConstraintSystemBuilder;
+use binius_core::{
+	constraint_system::validate::validate_witness, transparent::select_row::SelectRow,
+};
+use binius_field::{arch::OptimalUnderlier, BinaryField128b, BinaryField8b};
+
+type U = OptimalUnderlier;
+type F128 = BinaryField128b;
+type F8 = BinaryField8b;
+
+const LOG_SIZE: usize = 8;
+
+// SelectRow expects exactly one witness value at particular index to be set.
+fn main() {
+	let allocator = bumpalo::Bump::new();
+	let mut builder = ConstraintSystemBuilder::<U, F128>::new_with_witness(&allocator);
+
+	let index = 58;
+	assert!(index <= 1 << LOG_SIZE - 1);
+
+	let select_row = SelectRow::new(LOG_SIZE, index).unwrap();
+	let transparent = builder.add_transparent("select_row", select_row).unwrap();
+
+	if let Some(witness) = builder.witness() {
+		let mut transparent_witness = witness.new_column::<F8>(transparent);
+		let values = transparent_witness.as_mut_slice::<u8>();
+
+		values[index] = 0x01;
+	}
+
+	let witness = builder.take_witness().unwrap();
+	let constraints_system = builder.build().unwrap();
+
+	validate_witness(&constraints_system, &[], &witness).unwrap();
+}
